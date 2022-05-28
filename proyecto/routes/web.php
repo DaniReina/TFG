a2afe9cc6dbd75608618dd2dae6b91caf1ee920d
harols10 <harol88888@gmail.com>
2022-05-29 00:20:54 +0200

    migration

--- proyecto/routes/web.php
+++ proyecto/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -53,3 +54,5 @@ Route::get('/LOPD', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('student',StudentController::class);
